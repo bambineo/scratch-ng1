0bde1c7ab6e42ba18ffd51934134edd54cbf23ad
Bastian Rusche <bastian.rusche@googlemail.com>
2017-10-04 00:05:41 +0200

    browser-sync installed, introduced first controller

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-angular.module('BlankApp', ['ngMaterial']);
+
+var myApp = angular.module('myApp',['ngMaterial']);
+
+myApp.controller('mainController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+  $scope.color = 'Bluely';
+
+  $scope.sendForm = function() {
+      console.log('Test');
+  }
+
+}]);
