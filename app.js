2754f6557980e894cc85af23b463c6745f843cd6
Bastian Rusche <bastian.rusche@googlemail.com>
2017-10-08 20:07:34 +0200

    save work

--- app.js
+++ app.js
@@ -2,11 +2,26 @@
 var myApp = angular.module('myApp',['ngMaterial']);
 
 myApp.controller('mainController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
-  $scope.color = 'Bluely';
 
-  $scope.sendForm = function() {
-      console.log('Test');
+  $scope.todo = {};
+
+  $scope.sendForm = function($event) {
+      console.log($event);
+      newTodo = angular.copy($scope.todo);
+      $scope.todos.push(newTodo);
   }
+
+  $scope.todos = [
+    {title: 'erstes', description: 'echt erstes', done:true}, 
+    {title: 'zweites', description: 'echt zweites', done:false},
+    {title: 'drittes', description: 'echt drittes', done:true}
+  ];
+
+  $scope.deleteItem = function(index) {
+    console.log(index);
+    $scope.todos.splice(index, 1);
+  }
+
+
 
 }]);
